1ddb5033d0dba76ee07d600ed0d561e83bad89bf
Daniela <dbrailo@gmail.com>
2022-06-12 13:22:55 +0300

    filtered habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6 &&
+    planet["koi_steff"] > 2600 &&
+    planet["koi_steff"] < 10000
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
   .pipe(
@@ -11,11 +22,18 @@ fs.createReadStream("kepler_data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results);
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
